a8165a6d61e5802edc8fe393a771dc22eb560e3d
Dell <Dell@10.56.6.214>
2015-08-11 08:55:02 -0600

    Practica1 EDD

--- Practica1EDD_201314631/src/practica1edd_201314631/NodoDE.java
+++ Practica1EDD_201314631/src/practica1edd_201314631/NodoDE.java
@@ -11,20 +11,38 @@ package practica1edd_201314631;
  */
 public class NodoDE {
     public NodoDE anterior;
-    public int direccion;
+    public int tipo;
     public NodoDE siguiente;
     public String nombre;
-    
+    public int cantidad;
     
         public NodoDE(){}
         
-    public NodoDE(NodoDE anterior, int direccion, NodoDE siguiente){
+    public NodoDE(NodoDE anterior,String nombre, int tipo, int cantidad, NodoDE siguiente){
     
     this.anterior = anterior;
-    this.direccion = direccion;
+    this.nombre = nombre;
+    this.tipo = tipo;
+    this.cantidad = cantidad;
     this.siguiente = siguiente;
     
     }
+    
+    public int getCantidad(){
+    return cantidad;
+    }
+    public void setCantidad (int cantidad){
+        this.cantidad = cantidad;
+    }
+    
+    
+    public String getNombre(){
+    return nombre;
+    }
+    public void setNombre (String nombre){
+        this.nombre = nombre;
+    }
+    
     
     public NodoDE getAnterior(){
 
@@ -34,11 +52,11 @@ public class NodoDE {
     public void setAnterior(NodoDE anterior){
     this.anterior = anterior;
     }
-    public int getDireccion(){
-    return direccion;
+    public int getTipo(){
+    return tipo;
     }
-    public void setDireccion (int direccion){
-        this.direccion = direccion;
+    public void setTipo (int tipo){
+        this.tipo = tipo;
     }
     public NodoDE getSiguiente(){
     return siguiente;
